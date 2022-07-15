09240465d6b748d6bcb766e02986051840a69e95
abhinavsiwal <abhinavsiwal@gmail.com>
2022-07-15 16:36:33 +0530

    signup

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app=express();
 app.use(express.json());
+app.use(cors())
 
+//Import all the routes
+const user = require('./routes/user');
+
+app.use('/user',user);
 app.use('/',(req,res)=>{
     res.send("Server is running");
 })
---
+++ controllers/user-controller.js
@@ -0,0 +1,62 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.signup = async (req, res, next) => {
+  const { email, password } = req.body;
+  //   console.log(email,password);
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ message: "Signing up failed,Please Try again later" });
+  }
+  if (existingUser) {
+    return res
+      .status(422)
+      .json({ message: "User already exists. Please try to login" });
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const createUser = new User({
+    email,
+    password: hashedPassword,
+  });
+  try {
+    await createUser.save();
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ message: "Signing up falied, Please try again later" });
+  }
+  res.status(201).json({ success: true, userId: createUser._id });
+};
+
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ err: "Login failed, Please try again later." });
+  }
+  if (!existingUser) {
+    return res.status(401).json({ err: "Admin not found." });
+  }
+  let token;
+  try {
+    token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"365d"})
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({err:"JWT error."})
+  }
+  res.status(201).json({existingUser,token})
+};
