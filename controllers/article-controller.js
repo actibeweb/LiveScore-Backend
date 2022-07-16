df2cb341fa82559a7cacf13ac3e5d2a15e118bdc
abhinavsiwal <abhinavsiwal@gmail.com>
2022-07-16 13:27:55 +0530

    article  update

--- controllers/article-controller.js
+++ controllers/article-controller.js
@@ -9,5 +9,57 @@ exports.createArticle = async (req, res) => {
     console.log(err);
     return res.status(200).json({ err: "Errot in creating article" });
   }
-  res.status(200).json({ article });
+  res.status(200).json(article);
+};
+
+exports.getArticles = async (req, res) => {
+  let articles = [];
+  try {
+    articles = await Article.find();
+  } catch (err) {
+    return res.status(200).json({ err: "Fetching Products Failed" });
+  }
+  res.status(200).json(articles);
+};
+
+exports.getArticleById = async (req, res) => {
+  const articleId = req.params.id;
+  let article;
+  try {
+    article = await Article.findById(articleId);
+  } catch (err) {
+    console.log(err);
+    return res.status(200).json({ err: "Fetching Article Failed" });
+  }
+  if (!article) {
+    return res.status(200).json({ err: "Could not find article for this id" });
+  }
+  res.status(200).json(article);
+};
+
+exports.updateArticle = async (req, res) => {
+  const articleId = req.params.id;
+  let article;
+  try {
+    article = await Article.findById(articleId);
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(200)
+      .json({ err: "Something went wrong in getting Article" });
+  }
+  if (!product) {
+    return res.status(200).json({ err: "Could not find article for this id" });
+  }
+  try {
+    article = await Article.findByIdAndUpdate(articleId, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(200).json({ err: "Error in Updating product" });
+  }
+  res.status(200).json(article);
 };
