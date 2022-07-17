284320d18095c4f541f68e7f10b1c4741ba62e2c
abhinavsiwal <abhinavsiwal@gmail.com>
2022-07-17 17:02:07 +0530

    article image

--- controllers/article-controller.js
+++ controllers/article-controller.js
@@ -1,10 +1,27 @@
 const Article = require("../models/article");
+const cloudinary = require("cloudinary");
 
 exports.createArticle = async (req, res) => {
-  const { title, content } = req.body;
+  let imageLink = {};
+  try {
+    const result = await cloudinary.v2.uploader.upload(req.body.image, {
+      folder: "logo",
+      fetch_format: "auto",
+    });
+
+    imageLink = {
+      public_id: result.public_id,
+      url: result.secure_url,
+    };
+  } catch (err) {
+    console.log(err);
+    return res.status(200).json({ err: "Error in uploading image" });
+  }
+  console.log(imageLink);
+  req.body.image = imageLink;
   let article;
   try {
-    article = await Article.create({ title, content });
+    article = await Article.create(req.body);
   } catch (err) {
     console.log(err);
     return res.status(200).json({ err: "Errot in creating article" });
@@ -51,6 +68,25 @@ exports.updateArticle = async (req, res) => {
   if (!article) {
     return res.status(200).json({ err: "Could not find article for this id" });
   }
+
+  let imageLink = {};
+  try {
+    const result = await cloudinary.v2.uploader.upload(req.body.image, {
+      folder: "logo",
+      fetch_format: "auto",
+    });
+
+    imageLink = {
+      public_id: result.public_id,
+      url: result.secure_url,
+    };
+  } catch (err) {
+    console.log(err);
+    return res.status(200).json({ err: "Error in uploading image" });
+  }
+  console.log(imageLink);
+  req.body.image = imageLink;
+
   try {
     article = await Article.findByIdAndUpdate(articleId, req.body, {
       new: true,
@@ -65,26 +101,26 @@ exports.updateArticle = async (req, res) => {
 };
 
 exports.deleteArticle = async (req, res) => {
-    const articleId = req.params.id;
-    let article;
-    try {
-        article = await Article.findById(articleId);
-      } catch (err) {
-        console.log(err);
-        return res
-          .status(500)
-          .json({ err: "Something went wrong in delete Article" });
-      }
-      if (!article) {
-        return res
-          .status(500)
-          .json({ message: "Could not find article for this id" });
-      }
-      try {
-        await article.remove();
-      } catch (err) {
-        console.log(err);
-        return res.status(500).json({ message: "Article could not be deleted" });
-      }
-      res.status(200).json({ message: "Article deleted successfully" });
-}
+  const articleId = req.params.id;
+  let article;
+  try {
+    article = await Article.findById(articleId);
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ err: "Something went wrong in delete Article" });
+  }
+  if (!article) {
+    return res
+      .status(500)
+      .json({ message: "Could not find article for this id" });
+  }
+  try {
+    await article.remove();
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Article could not be deleted" });
+  }
+  res.status(200).json({ message: "Article deleted successfully" });
+};
--- models/article.js
+++ models/article.js
@@ -1,14 +1,24 @@
 const mongoose = require("mongoose");
 
 const articleSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  image: {
+    public_id: {
+      type: String,
+      // required: true,
     },
-    content: {
-        type: String,
-        required: true,
-    }
-})
+    url: {
+      type: String,
+      // required: true,
+    },
+  },
+});
 
 module.exports = mongoose.model("Article", articleSchema);
