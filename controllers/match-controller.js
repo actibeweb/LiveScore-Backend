39e75c56de7c06f140e2daaacc1919df00056694
abhinavsiwal <abhinavsiwal@gmail.com>
2022-07-20 21:46:12 +0530

    view and delete and update match

--- controllers/match-controller.js
+++ controllers/match-controller.js
@@ -10,3 +10,81 @@ exports.createMatch = async (req, res, next) => {
   }
   return res.status(200).json(match);
 };
+
+exports.getAllMatches = async (req, res, next) => {
+  let matches = [];
+  try {
+    matches = await Match.find();
+  } catch (err) {
+    console.log(err);
+    return res.status(200).json({ err: "Error getting matches" });
+  }
+  return res.status(200).json(matches);
+};
+
+exports.getMatchById = async (req, res, next) => {
+  const matchId = req.params.id;
+  let match;
+  try {
+    match = await Match.findById(matchId);
+  } catch (err) {
+    console.log(err);
+    return res.status(200).json({ err: "Fetching Match Failed" });
+  }
+  if (!match) {
+    return res.status(200).json({ err: "Could not find match for this id" });
+  }
+  res.status(200).json(match);
+};
+
+exports.updateMatch = async (req, res, next) => {
+  const matchId = req.params.id;
+  let match;
+  try {
+    match = await Match.findById(matchId);
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(200)
+      .json({ err: "Something went wrong in getting Match" });
+  }
+  if (!match) {
+    return res.status(200).json({ err: "Could not find match for this id" });
+  }
+  try {
+    match = await Match.findByIdAndUpdate(matchId, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(200).json({ err: "Error in Updating Match" });
+  }
+  res.status(200).json(match);
+};
+
+exports.deleteMatch = async (req, res, next) => {
+  const matchId = req.params.id;
+  let match;
+  try {
+    match = await Match.findById(matchId);
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ err: "Something went wrong in delete Match" });
+  }
+  if (!match) {
+    return res
+      .status(500)
+      .json({ message: "Could not find match for this id" });
+  }
+  try {
+    await match.remove();
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Match could not be deleted" });
+  }
+  res.status(200).json({ message: "Match deleted successfully" });
+};
--- routes/match.js
+++ routes/match.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 const { isAuthenticatedUser } = require("../middlewares/checkUserAuth");
 
-const { createMatch } = require("../controllers/match-controller");
+const { createMatch,getAllMatches,getMatchById,updateMatch,deleteMatch } = require("../controllers/match-controller");
 
 router.post("/create", isAuthenticatedUser, createMatch);
+router.get("/get", isAuthenticatedUser, getAllMatches);
+router.get("/get/:id", isAuthenticatedUser, getMatchById);
+router.put("/update/:id", isAuthenticatedUser, updateMatch);
+router.delete("/delete/:id", isAuthenticatedUser, deleteMatch);
 
 module.exports = router;
